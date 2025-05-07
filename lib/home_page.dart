ab4b64414c379ec897d2924840fb189ed09f5194
Idriss-rayan <rayanidriss27@gmail.com>
2025-05-07 18:14:41 +0600

    paragraph pdf

---
+++ lib/home_page.dart
@@ -0,0 +1,49 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:recipe/paragraph_pdf_api.dart';
+import 'package:recipe/save_and_open_pdf.dart';
+import 'package:recipe/simple_pdf_api.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () async {
+              final simplePdfFile = await SimplePdfApi.genearateSimpleTextPdf(
+                'Sample Text',
+                'Flutter Developper',
+              );
+              SaveAndOpenDocument.openPdf(simplePdfFile);
+            },
+            child: const Text("Simple Pdf"),
+          ),
+          SizedBox(height: 24),
+          ElevatedButton(
+            onPressed: () async {
+              final ParagraphPdf = await ParagraphPdfApi.generateParagraphPdf();
+              SaveAndOpenDocument.openPdf(ParagraphPdf);
+            },
+            child: const Text("Paragraph pdf"),
+          ),
+          SizedBox(height: 24),
+          ElevatedButton(
+            onPressed: () async {},
+            child: const Text("table Pdf"),
+          ),
+          SizedBox(height: 24),
+          ElevatedButton(
+            onPressed: () async {},
+            child: const Text("Image Pdf"),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:recipe/home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      home: HomePage(),
+    );
   }
 }
 
