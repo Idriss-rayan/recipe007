ce524da799f5411fa85e2b61ea9b82d7b3801e08
Idriss-rayan <rayanidriss27@gmail.com>
2025-06-01 22:22:25 +0600

    ad

--- lib/Learn_pdf/tester.dart
+++ lib/Learn_pdf/tester.dart
@@ -81,7 +81,7 @@ class _TesterState extends State<Tester> {
                 CustomCard(
                   name: 'idriss',
                   status: 'price',
-                  code: 'james',
+                  code: 'st patrick',
                 )
               ],
             )),
