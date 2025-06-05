200bc5f46bbc17894432d917201a883f470f9d8e
Idriss-rayan <rayanidriss27@gmail.com>
2025-06-05 12:22:08 +0600

    button dwn

--- lib/recipe_generate/pdf_recipe.dart
+++ lib/recipe_generate/pdf_recipe.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'package:flutter/material.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:recipe/Learn_pdf/save_and_open_pdf.dart';
